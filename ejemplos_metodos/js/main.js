2e5ea0d4e20aff4c8f0f39b31b5182549f207ad9
Y00w1 <dgomez1019@cue.edu.co>
2022-04-30 19:26:29 -0500

    practica
    Signed-off-by: Y00w1 <dgomez1019@cue.edu.co>

--- ejemplos_metodos/js/main.js
+++ ejemplos_metodos/js/main.js
@@ -18,5 +18,86 @@ function escr_input() {
 }
 
 //Ejemplo de element.getElementsByTagName(name)
+function cambiar(){
+    const lista = document.getElementsByTagName("UL")[0];
+    lista.getElementsByTagName("li")[0].innerHTML = "leche";
+}
 //retorna una colección de elementos hijos con una etiqueta dada;
 //retorna una lista de nodos
+
+//Ejemplo de document.createElement(name), Ejemplo de parentNode.appendChild(node) y element.innerHTML
+function crearelement(){
+    const elemparraf = document.createElement("p");//se crea el elemento p
+    elemparraf.innerText = "se creo el elemento p";//el innerText le modifica el contenido de texto interno
+    document.body.appendChild(elemparraf);//el appendChild agrega lo anteriormente creado al dom
+}
+
+//Ejemplo de element.style.left
+function moverIzq(){
+    document.getElementById("styleLeft").style.left = "30px";
+};
+function moverDer(){
+    document.getElementById("styleLeft").style.left = "-30px";
+}
+
+
+//ejemplo de element.setAttribute
+function set_attribute(){
+    document.getElementById("txt_link").setAttribute("href", "https://github.com/Davi1705/ws-algoritmos");
+}
+
+//ejemplo de element.element.getAttribute 
+function get_attribute(){
+    const atributo = document.getElementById("atributo");
+    let link = atributo.getAttribute("href");
+    alert(link);
+}
+
+//ejemplo de element.addEventListener 
+const boton8 = document.getElementById("btn8");
+boton8.addEventListener("mouseover", function() {
+    this.style.backgroundColor = "red";
+});
+boton8.addEventListener("click", function(){
+    document.getElementById("txt").innerHTML += "perdiste<br>"
+});
+boton8.addEventListener("mouseout", function() {
+    this.style.backgroundColor = "white";
+});
+
+//Ejemplo de window.scrollTo 
+const btn_9 = document.getElementById("btn9");
+btn_9.addEventListener("click", function() {
+    window.scrollTo(0,0);
+});
+
+//ejemplo de setTimeOut
+function esperar(){
+    timeout = setTimeout(alertf(), "3000");
+};
+function alertf(){
+    alert("Sorpresa");
+};
+
+//Ejemplo de setInterval
+setInterval(mitempo(), "1000");
+function mitempo(){
+    const date = new Date();
+    document.getElementById("interval").innerHTM = date.toLocaleTimeString();
+};
+
+//Ejemplo de confirm
+function confirm_f(){
+    confirm("Press a button!");
+};
+
+//Ejemplo de Arreglos: ForEach()
+let sum = 0;
+
+const numeros = [2,5,6,8,8];
+numeros.forEach(sumatoria);
+
+function sumatoria(item){
+    sum += item;
+}
+document.getElementById("sum").innerHTML = sum;
